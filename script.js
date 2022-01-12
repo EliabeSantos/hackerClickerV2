4f157e6e0d86f6657dca590551289997f2d4167d
Eliabe Santos <eliabedodo@gmail.com>
2022-01-12 12:45:19 -0300

    first hero added

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const Botao = document.getElementById("Botao");
 const contador = document.getElementById("contador");
 const contadorPs = document.getElementById("contador-ps");
+const contadorPC = document.getElementById("contador-pc");
 const hero01 = document.getElementById("hero-01");
+const hero01Value = document.getElementById("hero-01-value");
+const hero01Quantity = document.getElementById("hero-01-quantity");
 
 let count = 0;
 let acumulator = 2;
-
-setInterval(
-  function () {
-    contador.innerHTML = `Dinheiro: ${parseInt(count)}`;
-    contadorPs.innerHTML = `D/s: ${acumulator}`;
-  },
-  [1]
-);
+let click = 1;
 
 setInterval(
   function () {
@@ -21,23 +17,36 @@ setInterval(
   [10]
 );
 
-function addValue(x) {
-  count += 1;
+function addValue() {
+  count += click;
   return;
 }
 
 // Heros Secction
 
 let Hero01Power = 1;
+let Hero01Quantity = 0;
 let Hero01Value = 100;
 
 function MakeUpGradeHero01() {
   if (count >= Hero01Value) {
+    Hero01Quantity++;
     count -= Hero01Value;
-    Hero01Value = Hero01Value / 2 + Hero01Value;
+    Hero01Value += Hero01Value / 5;
     acumulator += Hero01Power;
   }
 }
 
 Botao.addEventListener("click", addValue);
 hero01.addEventListener("click", MakeUpGradeHero01);
+
+setInterval(
+  function () {
+    contador.innerHTML = `Dinheiro: ${parseInt(count)}`;
+    contadorPs.innerHTML = `Por Segundo: ${acumulator}`;
+    contadorPC.innerHTML = `Por Click: ${click}`;
+    hero01Value.innerHTML = parseInt(Hero01Value);
+    hero01Quantity.innerHTML = Hero01Quantity;
+  },
+  [1]
+);
